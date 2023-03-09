ec120c60c54a891b214046237ceb2414e6e2c10c
kareem_mostafa <kareemmostafa114@gmail.com>
2023-03-09 05:22:50 +0200

    validation done

--- src/app/students-form/students-form.component.ts
+++ src/app/students-form/students-form.component.ts
@@ -9,8 +9,48 @@ export class StudentsFormComponent {
   studentName:string='';
   studentAge:string='';
   students:{name:string,age:string}[]=[];
+  nameError:string=''
+  ageError:string=''
 
 onSubmit(){
-  this.students.push({name:this.studentName,age:this.studentAge})
+  if (!this.validateName(this.studentName)&&!this.validateAge(this.studentAge)){
+    console.log("df");
+    this.students.push({name:this.studentName,age:this.studentAge})
+  }
+
+}
+
+validateName(name:string){
+  const regex=new RegExp('^[a-zA-Z]{3,}$');
+  if (name==''){
+    this.nameError="name is required"
+
+    return true
+  }
+  if (!regex.test(name)){
+    this.nameError="name must be at least 3 characters"
+
+   return true
+  }
+  return false
+}
+validateAge(age:string){
+  const regex=new RegExp('^([0-9])*$');
+  if (age==''){
+    this.ageError="age is required"
+    return true
+  }
+  if (!regex.test(age)){
+    this.ageError="age must numbers only"
+
+   return true
+
+  }
+  if(parseInt(age)<=17){
+    this.ageError="age must be at least 18"
+
+    return true
+  }
+  return false
 }
 }
